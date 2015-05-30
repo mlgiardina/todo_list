12e37a284c321511c15422ae12f741d23da786fa
Matthew Giardina <mlgiardina@me.com>
2015-05-30 12:15:48 -0500

    setup initial display

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,39 +1,57 @@
+require_relative '../db/setup.rb'
+require_relative './todo.rb'
+
 class TodoList
 
-  def initialize(file_name)
-    @file_name = file_name
-
+  def initialize
+    @todos = Todo.all
   end
 
   def start
-    loop do
-      system('clear')
+    view_todos
+    display_options
+    get_input
+    do_stuff
 
-      puts "---- TODO.rb ----"
+  end
 
-      view_todos
+  def view_todos
+    puts "------To Do List------"
+    @todos.each { |item| puts item.entry }
+  end
 
-      puts
-      puts "What would you like to do?"
-      puts "1) Exit 2) Add Todo 3) Mark Todo As Complete"
-      print " > "
-      action = gets.chomp.to_i
-      case action
-      when 1 then exit
-      when 2 then add_todo
-      when 3 then mark_todo
-      else
-        puts "\a"
-        puts "Not a valid choice"
-      end
+  def do_stuff
+    case @input
+    when "1"
+      add_todo
+    when "2"
+      puts "Which entry would you like to mark as completed?"
+    when "3"
+      puts "Which entry would you like to delete?"
+    when "4"
+      puts "Goodbye!"
+      exit
+    else
+      puts "That's not a valid option"
     end
   end
 
-  def todos
-    @todos
+  def display_options
+    puts "\n\nWhat would you like to do?\n"
+    puts "(1) Add an entry"
+    puts "(2) Mark an entry as completed"
+    puts "(3) Delete Todo"
+    puts "(4) Quit"
+  end
+
+  def add_todo
+    Todo.create(entry: response)
   end
 
   def get_input
-    gets.chomp
+    @input = gets.chomp
   end
+
 end
+
+TodoList.new.start
